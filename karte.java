aca1003668e3aa239cf859b5fc574e18722f7b7b
Aso2001390 <82856125+Aso2001390@users.noreply.github.com>
2021-06-17 12:25:55 +0900

    Update karte.java

--- karte.java
+++ karte.java
@@ -1,22 +1,13 @@
 public class DrSystem {
-    public static void main(String[] args){
-        
-        // 病院名はKarteクラス共通
+    public static void main(String[] args)
         Karte.hospitalName = "福岡病院";
         Karte.hospitalTel = "092-1234-5678";
-
-        // Karte型変数を宣言してインスタンス領域を確保する
         Karte karte1 = new Karte();
         Karte karte2 = new Karte();
-
-        // １人目の患者のカルテ
         karte1.id = 1123; karte1.name = "太郎";
-        // ２人目の患者のカルテ
         karte2.id = 1124; karte2.name = "次郎";
-
         System.out.printf("病院名:%s 電話番号:%s id: %d 名前: %s \n", 
             Karte.hospitalName, Karte.hospitalTel, karte1.id, karte1.name);
-            
         System.out.printf("病院名:%s 電話番号:%s id: %d 名前: %s \n", 
             Karte.hospitalName, Karte.hospitalTel, karte2.id, karte2.name);
     }    
